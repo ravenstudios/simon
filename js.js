ba1ab3f1c3ff8a514627e261e74ffac129904120
ravenstudios <ravenstudiosrob@yahoo.com>
2017-10-23 14:18:07 -0500

    logic done

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
 console.log("Simon");
 
-let pattern = [];
-let gameStep = 10;
-let currentSetp;
+let pattern;
+let gameStep = 0;
+let currentStep = -1;
+let playerSteps= 0;
 let lastButtonPushed;
 let gameStart;
-
+let canPush = false;
+let strict = false;
 let buttons = [".greenGameButton", ".redGameButton", ".blueGameButton", ".yellowGameButton"];
 let initColors = ["rgba(0, 255, 0, 0.5)", "rgba(255, 0, 0, 0.5)", "rgba(0, 0, 255, 0.5)", "rgba(255, 255, 0, 0.5)"];
 let selectedColors = ["rgba(50, 255, 50, 1)", "rgba(255, 50, 50, 1)", "rgba(50, 50, 255, 1)", "rgba(255, 255, 50, 1)"];
@@ -17,41 +19,92 @@ let soundsLink = [
 ];
 
 let sounds = [];
+let power = false;
 
 $(()=>{
   console.log("jquery");
 
-  setup();
+  reset()
 
   $(".greenGameButton").mousedown(()=>{
-    $(buttons[0]).css("background-color", selectedColors[0]);
-    playButton(0);
+    if(canPush){
+      pressButton(0);
+    }
+
   });
 
   $(".redGameButton").mousedown(()=>{
-    $(buttons[1]).css("background-color", selectedColors[1]);
-    playButton(1);
+    if(canPush){
+      pressButton(1);
+    }
+
   });
 
   $(".blueGameButton").mousedown(()=>{
-    $(buttons[2]).css("background-color", selectedColors[2]);
-    playButton(2);
+    if(canPush){
+      pressButton(2);
+    }
+
   });
 
   $(".yellowGameButton").mousedown(()=>{
-    $(buttons[3]).css("background-color", selectedColors[3]);
-    playButton(3);
+    if(canPush){
+      pressButton(3);
+    }
   });
 
-  $(".gameButton").mouseup(()=>{
-    //resetColors();
+  $("#onOff").click(()=>{
+    console.log("on off");
+
+    if(power){
+      //unit is on and were turning it off
+      $("#onOff").css("background-color", "rgba(255, 0, 0, 0.5)");
+      power = false;
+      reset();
+      canPush = false;
+      counter();
+    }
+
+    else{
+      //unit is off and were turning it on
+      $("#onOff").css("background-color", "rgba(255, 0, 0, 1)");
+      power = true;
+      reset();
+      playPattern();
+    }
   });
 
-  playPattern();
+  $("#reset").click(()=>{
+    console.log("reset");
+    reset();
+    playPattern();
+  });
+
+  $("#strict").click(()=>{
+    console.log("strict");
+
+    if(strict){
+      //strict is on and were turning it off
+      $("#strict").css("background-color", "rgba(255, 0, 0, 0.5)");
+      strict = false;
+    }
+
+    else{
+      //strict is off and were turning it on
+      $("#strict").css("background-color", "rgba(255, 0, 0, 1)");
+      strict = true;
+    }
+
+  });
+
+
+
 })
 
-function setup(){
-  for (var i = 0; i < 100; i++) {
+function reset(){
+  counter();
+  pattern = [];
+  for (var i = 0; i < 20; i++) {
     let rand = Math.floor(Math.random() * 4);
     pattern.push(rand)
   }
@@ -61,22 +114,91 @@ function setup(){
     sounds.push(new Audio(item));
   })
   resetColors();
+  gameStep = 0;
+  currentStep = -1;
 }
 
 function pressButton(btn){
-  //if correct button is pressed replay pattern up to current step
 
-  //else end game
+
+
+  playButton(btn)
+
+  if(btn === pattern[currentStep + 1]){//correct
+    console.log("right");
+    console.log("currentStep: " + currentStep);
+    console.log("gameStep: " + gameStep);
+
+    currentStep ++;
+
+    if(currentStep === gameStep){
+      setTimeout(function() {
+
+        gameStep++;
+        counter();
+        currentStep = -1;
+        playPattern();
+      }, 500);
+    }
+
+
+  }
+  else{//incorrect
+    console.log("wrong");
+
+    initColors.forEach((item, index)=>{
+      $(buttons[index]).css("background-color", selectedColors[index]);
+    });
+
+    setTimeout(function() {
+      resetColors();
+    }, 500);
+
+
+    if(strict){
+
+
+      setTimeout(function() {
+        reset();
+        playPattern();
+      }, 1500);
+    }
+    else{
+
+
+      //play error sound
+      setTimeout(function() {
+        currentStep = -1;
+        playPattern();
+      }, 500);
+
+    }
+
+  }
+
 }
 
+function counter(){
+  if(gameStep < 10){
+    $("#counter").val("0" + gameStep);
+  }
+  else{
+    $("#counter").val(gameStep);
+  }
+}
 function playPattern(){
-
+  canPush = false;
   let i = 0;
 
 
     let next = setInterval(function() {
-      console.log(i);
+
       if(i === gameStep){
+        clearInterval(next);
+        canPush = true;
+      }
+
+      if(!power){
         clearInterval(next);
       }
       playButton(pattern[i])
@@ -91,11 +213,13 @@ function playPattern(){
   }
 
   function playButton(btn){
+    canPush = false;
     $(buttons[btn]).css("background-color", selectedColors[btn]);
     sounds[btn].play();
 
     setTimeout(function() {
       resetColors();
+      canPush = true;
     }, 500);
 
 
